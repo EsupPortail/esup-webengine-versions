ca2cd220627f0624e8b16eab4235b013c9217851
vbonamy <vbonamy@2bebc3db-464a-0410-8034-b528f6c09253>
2009-04-28 15:17:35 +0000

    esup-webengine-versions : resolve uid ok ...
    git-svn-id: https://subversion.renater.fr/esup-ecm/esup-webengine-versions/trunk@284 2bebc3db-464a-0410-8034-b528f6c09253

--- src/main/java/org/esupportail/ecm/versions/Main.java
+++ src/main/java/org/esupportail/ecm/versions/Main.java
@@ -3,16 +3,28 @@ package org.esupportail.ecm.versions;
 import java.io.*;
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.nuxeo.ecm.core.api.ClientException;
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.DocumentRef;
+import org.nuxeo.ecm.core.api.IdRef;
 import org.nuxeo.ecm.core.rest.*;
 import org.nuxeo.ecm.webengine.model.*;
 import org.nuxeo.ecm.webengine.model.impl.*;
 import org.nuxeo.ecm.webengine.model.exceptions.*;
 import org.nuxeo.ecm.webengine.*;
 
+import sun.util.logging.resources.logging;
+
 @WebObject(type="esupversions")
 @Produces("text/html; charset=UTF-8")
 public class Main extends ModuleRoot {
 
+  private static final Log log = LogFactory.getLog(Main.class);
+	
   /**
    * Default view
    */
@@ -21,23 +33,28 @@ public class Main extends ModuleRoot {
     return getView("index");
   }
 
-  /**
-   * Return the template index1.ftl from 'skin' directory
-   */
   @GET
-  @Path("index1")
-  public Object getIndex1() {
-    return getTemplate("index1.ftl");
+  @Path("uid/{versionUid}")
+  public Object getVersion(@PathParam("versionUid") String versionUid) {
+	
+	Template returnedView = getView("resolve").arg("versionUid", versionUid);
+	  
+	CoreSession session = ctx.getCoreSession();
+	final DocumentRef docRef = new IdRef(versionUid);
+	
+	try {
+		DocumentModel doc = session.getDocument(docRef);
+		returnedView.arg("version", doc);
+		
+		return 	DocumentFactory.newDocumentRoot(ctx, docRef);
+	} catch(ClientException ce) {
+		log.error("Version document can't be viewed", ce);
+		returnedView.arg("error", "Version document can't be viewed : " + ce.getMessage());
+	}
+	
+	return returnedView;
   }
 
-  /**
-   * Default view
-   */
-  @GET
-  @Path("{name}")
-  public Object doGet(@PathParam("name") String name) {
-    return getView("index").arg("name", name);
-  }
   
 }
 
